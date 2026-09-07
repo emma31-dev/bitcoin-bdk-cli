c294b0c0e9f6942d3668ad18ba91f683d43d3f60
emma31-dev <emmanuelfidel07@gmail.com>
2026-09-07 14:51:07 +0100

    Reformat persisted wallet error message

--- src/utils/runtime.rs
+++ src/utils/runtime.rs
@@ -61,7 +61,9 @@ impl RuntimeWallet {
                 if wallet.persist(persister)? {
                     Ok(())
                 } else {
-                    Err(Error::Generic("Wallet changes were not persisted".to_string()))
+                    Err(Error::Generic(
+                        "Wallet changes were not persisted".to_string(),
+                    ))
                 }
             }
         }
